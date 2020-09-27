4c647730661aca5b826871a478b160eabf69c29f
sree-chikati <srechi453.fusdk12@gmail.com>
2020-09-26 18:17:20 -0700

    prints poem file backwards (but no number yet)

---
+++ poetry-slam.py
@@ -0,0 +1,17 @@
+def get_file_lines(filename):
+    file_lines = open (filename, "r").read()
+    return file_lines
+print(get_file_lines("poem.txt"))
+
+print(" ")
+
+def lines_printed_backwards(line_list):
+    poem_dict_lines = open (line_list, "r").readlines()
+    reverse = ''
+    for line in poem_dict_lines:
+        reverse = line + reverse
+    return reverse
+print(lines_printed_backwards("poem.txt"))
+    
+#"the lines of the poem in reverse. Include the original line number at the beginning of each line.")
+
