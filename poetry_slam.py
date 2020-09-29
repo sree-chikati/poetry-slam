2ce4059f5be882e263718aaee3726d74a99216a6
sree-chikati <srechi453.fusdk12@gmail.com>
2020-09-29 10:36:28 -0700

    made changes according to pylint

--- poetry_slam.py
+++ poetry_slam.py
@@ -1,17 +1,19 @@
 def get_file_lines(filename):
-    file_lines = open (filename, "r").read()
+    file_lines = open(filename, "r").read()
     return file_lines
 print(get_file_lines("poem.txt"))
 
 print(" ")
 
 def lines_printed_backwards(line_list):
-    poem_dict_lines = open (line_list, "r").readlines()
+    poem_dict_lines = open(line_list, "r").readlines()
     reverse = ''
     for line in poem_dict_lines:
         reverse = line + reverse
     return reverse
 print(lines_printed_backwards("poem.txt"))
-    
-#"the lines of the poem in reverse. Include the original line number at the beginning of each line.")
+"""
+the lines of the poem in reverse. 
+Include the original line number at the beginning of each line.
+"""
 
