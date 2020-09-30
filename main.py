bbb9dfff0b841b5afbe8b94ab9726fd5c91ebd8a
sree-chikati <srechi453.fusdk12@gmail.com>
2020-09-29 19:27:51 -0700

    renamed poetry_slam.py to main.py, added another poem

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#importing random(). This allows us to get a random selection of things
+from random import randint
+
 def get_file_lines(filename):
     """
     Reaturns a list of strings containing the lines of the file
@@ -7,7 +10,7 @@ def get_file_lines(filename):
     file_lines.close()
     return poem_lines
     
-print(get_file_lines("poem.txt"))
+print(get_file_lines("poem1.txt"))
 
 print(" ")
 
@@ -19,12 +22,23 @@ def lines_printed_backwards(line_list):
     line_list = line_list[::-1]
     line_num = len(line_list)
 
-    for i in range(len(line_list)):
+    for i in range(line_num):
         reverse = str(line_num - i) + " " + line_list[i]
         print(reverse)
 
-poem = open('./poem.txt', 'r')
+poem = open('./poem1.txt', 'r')
 lines_printed_backwards(poem.read().splitlines())
 
+print(" ")
+
 def lines_printed_random(line_list):
+    line_num = len(line_list)
+    for i in range(line_num):
+        rand = line_list[randint(i, line_num-1)]
+        print(rand)
+
+poem = open('./poem1.txt', 'r')
+lines_printed_random(poem.read().splitlines())
+
+def lines_printed_custom(line_list):
     return
