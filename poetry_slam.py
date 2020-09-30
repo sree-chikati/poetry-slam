4bb59a70751b9fc3a3b71e650bb00d7349ad6ec0
sree-chikati <srechi453.fusdk12@gmail.com>
2020-09-29 18:24:49 -0700

    completed lines_printed_backwards

--- poetry_slam.py
+++ poetry_slam.py
@@ -2,22 +2,27 @@ def get_file_lines(filename):
     """
     Reaturns a list of strings containing the lines of the file
     """
-    file_lines = open(filename, "r").read()
-    return file_lines
+    file_lines = open(filename, "r")
+    poem_lines = file_lines.read().splitlines()
+    file_lines.close()
+    return poem_lines
+    
 print(get_file_lines("poem.txt"))
 
 print(" ")
 
-def lines_printed_backwards(line_list):
+def lines_printed_backwards(filename):
     """
     The lines of the poem in reverse. 
     Include the original number at the beginning of each line
     """
-    poem_dict_lines = open(line_list, "r").readlines()
-    reverse = ''
-    count = len(line_list)
-    for line in poem_dict_lines:
-        count = count -1
-        reverse += str(count) + " " + line
-    return reverse
-print(lines_printed_backwards("poem.txt"))
+    f = open(filename, 'r')
+    line_list = f.read().splitlines()
+    line_num = len(line_list)
+    line_list = line_list[::-1]
+
+    for i in range(len(line_list)):
+        reverse = str(line_num - i) + " " + line_list[i]
+        print(reverse)
+
+lines_printed_backwards("./poem.txt")
