8ab66b214a6821e8e4eb3d6ea8d6440c0119c228
sree-chikati <srechi453.fusdk12@gmail.com>
2020-09-29 19:57:03 -0700

    created a custom function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #importing random(). This allows us to get a random selection of things
 from random import randint
 
+# FUNCTION 1----------------------------------------------------
 def get_file_lines(filename):
     """
     Reaturns a list of strings containing the lines of the file
@@ -14,10 +15,11 @@ print(get_file_lines("poem1.txt"))
 
 print(" ")
 
+# FUNCTION 2----------------------------------------------------
 def lines_printed_backwards(line_list):
     """
-    The lines of the poem in reverse. 
-    Include the original number at the beginning of each line
+    The lines of the poem will be printed in reverse order. 
+    Includes the original number at the beginning of each line
     """
     line_list = line_list[::-1]
     line_num = len(line_list)
@@ -31,7 +33,11 @@ lines_printed_backwards(poem.read().splitlines())
 
 print(" ")
 
+# FUNCTION 3----------------------------------------------------
 def lines_printed_random(line_list):
+    """
+    The lines of the poem will be printed in a random order.
+    """
     line_num = len(line_list)
     for i in range(line_num):
         rand = line_list[randint(i, line_num-1)]
@@ -40,5 +46,18 @@ def lines_printed_random(line_list):
 poem = open('./poem1.txt', 'r')
 lines_printed_random(poem.read().splitlines())
 
+print(" ")
+
+# FUNCTION 4----------------------------------------------------
 def lines_printed_custom(line_list):
-    return
+    """
+    The lines of the poem will be printed in a alphabetical order.
+    """
+    #sorted function return a sorted alchabetically ordered list.
+    # Here, we are reversing the alphabetical sort with reverse=True. 
+    # So, it will return a descending alphabetically orded list
+    for line in sorted(line_list, reverse=True):
+        #end='' will end the line with a space
+        print(line, end='')
+poem = open('./poem1.txt', 'r')
+lines_printed_custom(poem.readlines())
